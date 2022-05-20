6c6414fe5cf6086bd1846e2b230a7d5f9eb9dafd
Anton Korneta <korneta.anton@transferwise.com>
2022-05-20 14:28:04 +0300

    Close db conn when tx related error happen (#7)

--- src/test/java/com/transferwise/common/gaffer/test/complextest1/app/ClientsService.java
+++ src/test/java/com/transferwise/common/gaffer/test/complextest1/app/ClientsService.java
@@ -1,11 +1,15 @@
 package com.transferwise.common.gaffer.test.complextest1.app;
 
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("clientsService")
 public class ClientsService {
+
+  protected static final Logger log = LoggerFactory.getLogger(ClientsService.class);
 
   @Resource(name = "clientsDAO")
   private ClientsDao clientsDao;
@@ -32,5 +36,15 @@ public class ClientsService {
     clientsDao.createAccount(accountId, clientId, "666");
 
     usersService.createUser(clientId, "bemmimehed@tallinn.ee");
+  }
+
+  @Transactional
+  public void deleteClient(String clientName) {
+    try {
+      clientsDao.deleteClient(clientName);
+    } catch (ClientNotFoundException ex) {
+      log.error("client not found", ex);
+    }
+    clientsDao.deleteAccount("666");
   }
 }
